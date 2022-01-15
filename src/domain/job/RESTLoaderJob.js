aac315b56dacd2b970100362edbd9c3139ff0ff7
Sebastian Montero <sebastian.montero@gmail.com>
2022-01-15 17:25:26 -0600

    Added log statements to the RESTLoaderJob class.

--- src/domain/job/RESTLoaderJob.js
+++ src/domain/job/RESTLoaderJob.js
@@ -2,6 +2,7 @@ const axios = require('axios').default
 const LoaderJob = require('./LoaderJob')
 const { RESTAuthHandlerFactory } = require('../auth')
 const { ExternalError, InternalError } = require('../../error')
+const { logger } = require('../../service')
 
 class RESTLoaderJob extends LoaderJob {
   async _fetchPayload (retries = 1) {
@@ -32,8 +33,10 @@ class RESTLoaderJob extends LoaderJob {
       return response.data
     } catch (error) {
       const errorMsg = `failed fetching payload for job: ${JSON.stringify(this.config, null, 4)}\n`
+      logger.info(`Error object: ${JSON.stringify(error, null, 4)}`)
       if (error.response) {
         const { response } = error
+        logger.info(`Error object response: ${JSON.stringify(response, null, 4)}`)
         if (authHandler) {
           if (retries > 0 && authHandler.isRecoverableAuthError(response.data, auth)) {
             return this._fetchPayload(retries - 1)
@@ -45,6 +48,7 @@ class RESTLoaderJob extends LoaderJob {
           }
         }
       } else if (error.request) {
+        logger.info(`Error object request: ${JSON.stringify(error.request, null, 4)}`)
         // Server error
         // errorMsg += `error: ${JSON.stringify(error.request, null, 4)}\n`
         throw new ExternalError(errorMsg, error)
