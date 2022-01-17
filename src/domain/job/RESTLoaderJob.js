05cf81f28a9f5cf53dbbd53369af9b1cc9267558
Sebastian Montero <sebastian.montero@gmail.com>
2022-01-16 18:53:45 -0600

    Installed json-stringify-safe package. Updated RESTLoaderJob class to use the safe stringify function to stringifiy objects

--- src/domain/job/RESTLoaderJob.js
+++ src/domain/job/RESTLoaderJob.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default
+const stringify = require('json-stringify-safe')
 const LoaderJob = require('./LoaderJob')
 const { RESTAuthHandlerFactory } = require('../auth')
 const { ExternalError, InternalError } = require('../../error')
@@ -32,25 +33,25 @@ class RESTLoaderJob extends LoaderJob {
       const response = await axios(requestConfig)
       return response.data
     } catch (error) {
-      const errorMsg = `failed fetching payload for job: ${JSON.stringify(this.config, null, 4)}\n`
-      logger.info(`Error object: ${JSON.stringify(error, null, 4)}`)
+      const errorMsg = `failed fetching payload for job: ${stringify(this.config, null, 4)}\n`
+      logger.info(`Error object: ${stringify(error, null, 4)}`)
       if (error.response) {
         const { response } = error
-        logger.info(`Error object response: ${JSON.stringify(response, null, 4)}`)
+        logger.info(`Error object response: ${stringify(response, null, 4)}`)
         if (authHandler) {
           if (retries > 0 && authHandler.isRecoverableAuthError(response.data, auth)) {
             return this._fetchPayload(retries - 1)
           } else {
             // Server error
-            // errorMsg += `error: ${JSON.stringify(response, null, 4)}\n`
+            // errorMsg += `error: ${stringify(response, null, 4)}\n`
             // console.log(errorMsg)
             throw new ExternalError(errorMsg, error)
           }
         }
       } else if (error.request) {
-        logger.info(`Error object request: ${JSON.stringify(error.request, null, 4)}`)
+        logger.info(`Error object request: ${stringify(error.request, null, 4)}`)
         // Server error
-        // errorMsg += `error: ${JSON.stringify(error.request, null, 4)}\n`
+        // errorMsg += `error: ${stringify(error.request, null, 4)}\n`
         throw new ExternalError(errorMsg, error)
       } else {
         // Error setting up request, code or config issue should rethrow
